8dc242b8943a7e1880bd5556e0fd89415720ff42
NarutoBratishka <katorabian@gmail.com>
2022-12-05 00:10:16 +0700

    base: add: simple dependency inject variant

--- app/src/main/java/com/example/dependencyinjectionstart/example1/Activity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/Activity.kt
@@ -2,5 +2,10 @@ package com.example.dependencyinjectionstart.example1
 
 class Activity {
 
-    val computer: Computer = Component().getComputer()
+    lateinit var computer: Computer
+    lateinit var keyboard: Keyboard
+
+    init {
+        Component().inject(this)
+    }
 }
