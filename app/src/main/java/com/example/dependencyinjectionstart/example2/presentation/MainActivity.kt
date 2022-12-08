ec24e6a69605e08f611aedb9447294e7a84c2d14
NarutoBratishka <katorabian@gmail.com>
2022-12-09 04:06:19 +0700

    base: add: singleton realization

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.dependencyinjectionstart.example2.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dependencyinjectionstart.R
+import com.example.dependencyinjectionstart.example2.ExampleApplication
 import com.example.dependencyinjectionstart.example2.di.DaggerApplicationComponent
 import javax.inject.Inject
 
@@ -11,10 +13,7 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var viewModel: ExampleViewModel
 
-    private val component by lazy {
-        DaggerApplicationComponent.factory()
-            .create(application, System.currentTimeMillis())
-    }
+    private val component by lazy { (application as ExampleApplication).component }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         component.inject(this)
