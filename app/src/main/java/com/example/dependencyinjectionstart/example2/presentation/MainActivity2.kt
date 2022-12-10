4d5bc04097d70667d5b84847ca53407b593c40a5
NarutoBratishka <katorabian@gmail.com>
2022-12-10 08:56:09 +0700

    base: change: viewModelFactory to provide new instances on another owner

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity2.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity2.kt
@@ -1,9 +1,7 @@
 package com.example.dependencyinjectionstart.example2.presentation
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.dependencyinjectionstart.R
@@ -11,7 +9,7 @@ import com.example.dependencyinjectionstart.example2.ExampleApplication
 import com.example.dependencyinjectionstart.example2.di.DaggerApplicationComponent
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
@@ -31,10 +29,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         viewModel.method()
         viewModel2.method()
-        findViewById<TextView>(R.id.tv_hallo_world)?.setOnClickListener {
-            Intent(this, MainActivity2::class.java).also {
-                startActivity(it)
-            }
-        }
     }
 }
